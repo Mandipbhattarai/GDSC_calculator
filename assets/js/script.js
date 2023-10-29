7b2ab28e74b83082540d2b4b826378a3b86ac9f3
mr-osiris <125508995+mr-osiris@users.noreply.github.com>
2023-10-29 21:59:44 +0530

    i have introduced mathematical operation like sin cos tan log ln square and squareroot

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,68 @@ let value = document.getElementById('value');
 let toggleBtn = document.querySelector('.toggleBtn');
 let body = document.querySelector('body');
 
-
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function () {
-        if (this.innerHTML == "=") {
-            value.innerHTML = eval(value.innerHTML);
-        }
-        else {
-            if (this.innerHTML == "Clear") {
-                value.innerHTML = "";
+        if (this.innerHTML === "=") {
+            try {
+                value.innerHTML = eval(value.innerHTML);
+            } catch (error) {
+                value.innerHTML = 'Error';
             }
-            else {
-                value.innerHTML += this.innerHTML;
+        } else if (this.innerHTML === "Clear") {
+            value.innerHTML = "";
+        } else if (this.innerHTML === "√") {
+            if (isValidInput(value.innerHTML) && eval(value.innerHTML) >= 0) {
+                value.innerHTML = Math.sqrt(eval(value.innerHTML));
+            } else {
+                value.innerHTML = 'Invalid Input';
             }
+        } else if (this.innerHTML === "x²") {
+            if (isValidInput(value.innerHTML)) {
+                value.innerHTML = eval(value.innerHTML) ** 2;
+            } else {
+                value.innerHTML = 'Invalid Input';
+            }
+        } else if (this.innerHTML === "sin") {
+            if (isValidInput(value.innerHTML)) {
+                value.innerHTML = Math.sin((Math.PI / 180) * eval(value.innerHTML));
+            } else {
+                value.innerHTML = 'Invalid Input';
+            }
+        } else if (this.innerHTML === "cos") {
+            if (isValidInput(value.innerHTML)) {
+                value.innerHTML = Math.cos((Math.PI / 180) * eval(value.innerHTML));
+            } else {
+                value.innerHTML = 'Invalid Input';
+            }
+        } else if (this.innerHTML === "tan") {
+            if (isValidInput(value.innerHTML)) {
+                value.innerHTML = Math.tan((Math.PI / 180) * eval(value.innerHTML));
+            } else {
+                value.innerHTML = 'Invalid Input';
+            }
+        } else if (this.innerHTML === "log") {
+            if (isValidInput(value.innerHTML) && eval(value.innerHTML) > 0) {
+                value.innerHTML = Math.log10(eval(value.innerHTML));
+            } else {
+                value.innerHTML = 'Invalid Input';
+            }
+        } else if (this.innerHTML === "ln") {
+            if (isValidInput(value.innerHTML) && eval(value.innerHTML) > 0) {
+                value.innerHTML = Math.log(eval(value.innerHTML));
+            } else {
+                value.innerHTML = 'Invalid Input';
+            }
+        } else {
+            value.innerHTML += this.innerHTML;
         }
-    })
+    });
 }
-
-
 
 toggleBtn.onclick = function () {
     body.classList.toggle('dark');
+}
+
+function isValidInput(input) {
+    return !isNaN(eval(input));
 }
